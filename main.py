21a5631ee201f8883f84b012aa43d06a5b8c0c55
ksel <kevselen@gmail.com>
2021-09-12 00:05:02 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import socket
+import platform
+import getpass
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('google.com', 80))
+        ip = s.getsockname()[0]
+        s.close()
+    except:
+        ip = 'N/A'
+    return ip
+    print(ip)
+
+print('*IP ADDRESS: ' + get_local_ip())
+print('*PROCESSOR: ' + platform.machine())
+
+os = platform.system()
+if os == 'Darwin':
+    os_name = 'MACOS '
+    print('*OS: ' + os_name + platform.mac_ver()[0])
+else:
+    print(os + ' ' + platform.version())
+
+print('*USER: ' + getpass.getuser())
